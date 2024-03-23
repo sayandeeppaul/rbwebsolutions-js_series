650412144dd9454a7d6f087994cc2de3bd8e5c2f
Sayandeep Paul <sayandeep775@gmail.com>
2024-03-23 22:20:28 +0530

    code pushed

--- 15_lecture/execution.js
+++ 15_lecture/execution.js
@@ -10,19 +10,21 @@
 // let result2 = addTwo(50,500)
 
 function one() {
-    console.log("one");
-    two();
-  }
-  
-  function two() {
-    console.log("two");
-    three();
-  }
-
-  function three() {
-    console.log("three");
-  }
-  
-  one();
+  console.log("one");
   two();
+  console.log("1");
+}
+
+function two() {
+  console.log("two");
   three();
+  console.log("2");
+}
+
+function three() {
+  console.log("three");
+}
+
+one();
+two();
+three();
